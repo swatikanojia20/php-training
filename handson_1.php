9b60cc76ae03e0566e2d62e4ff43fbc22b5cf923
Varun Ved <24x7varun@gmail.com>
2014-10-30 20:30:56 +0530

    Added handson_1.php

---
+++ handson_1.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+* Author : Varun
+* Date   : 30-May-2014
+* Title  : PHP Hands-on - 1
+* Desc   : Basics (Variable, if, for)
+*/
+
+/*Write a PHP code to print Mathematical Table for given number '$tablefor'.
+  Also validate the value of $tablefor, it should be always greater than 0.
+  If its '0' then show proper error message and dont print the table. 
+  Variables and basic skeleton is already created for you.
+*/
+
+//$tablefor is input for generating table for its value.
+$tablefor = 2;
+
+if($tablefor<=0)
+{
+	echo "Please enter the value of tablefor greater than zero.";
+}
+else
+{
+	for($index=1; $index<=10; index++)
+	{
+		$multiple = $tablefor*$index;
+		echo "$tablefor X $index = $multiple";
+	}
+}
+
+
+
+ ?>
