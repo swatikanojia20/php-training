c89531172c7701ed9587805cf0bdaa020ed3d77e
Swati Kanojia <swatikanojia20@gmail.com>
2014-11-11 23:34:44 +0530

    Update handson_2.php

--- handson_2.php
+++ handson_2.php
@@ -11,7 +11,7 @@
 * appropriate message to the users.
 * You can save users input in a variable and hardcode the answer in it. 
 * Basic Skeleton has already been created, just push your code at right place. 
-* Quiz Question : Who is the winner of ICC WORLD CUP 2012?
+* Quiz Question : Who is the winner of ICC WORLD CUP 2011?
 * Options : Pakistan, Sri Lanka, India, England 
 */
 
@@ -19,21 +19,43 @@
 
 
 //Declare an answer variable and hardcode the correct answer in it.
-
+$answer = "India";
 
 //Write echo statement to print question to user.
-
-
+echo "Who is the winner of ICC WORLD CUP 2011?";
+echo "<br>";
 //Write echo statement to print all options
 
+Switch($answer){
+
+ case 'Pakistan':
+ 	echo "Pakistan is a wrong answer";
+ 	break;
+
+ 	case 'Sri Lanka':
+ 	 echo "Sri lanka is a wrong answer";
+ 		break;
+ 		case 'India':
+ 			echo "India is a right answer";
+ 			break;
+ 			case 'England':
+ 				echo "England is a wrong answer";
+ 				break;
 
 
-
-
+}
+echo "<br>";
 
 //Write logic to find whether given answer is correct or not using if else.
 
+if ($answer=="Pakistan") {
+	echo "This answer is correct";
+}
+else{
 
+echo "This is a wrong answer";
+
+}
 
   
 ?>
