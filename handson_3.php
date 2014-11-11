0ef5ce9a3d928b7499f6fd30997e2d350c604d68
Swati Kanojia <swatikanojia20@gmail.com>
2014-11-11 23:58:03 +0530

    Update handson_3.php

--- handson_3.php
+++ handson_3.php
@@ -5,7 +5,6 @@
 * Title  : PHP Hands-on - 3
 * Desc   : DiveIn (Constant, Control Structures, Array)
 */
-
 /*
 * Write a PHP code to create a quiz. Implement Double-Dip logic
 * Double-Dip Means : User can choose two answers. He will win if any of the 2 answer is correct.
@@ -16,26 +15,18 @@
 * Quiz Question : Who is the winner of ICC WORLD CUP 2012?
 * Options : Pakistan, Sri Lanka, India, England 
 */
-
 //Define a constant below
-
-
 //Declare an answer array and hardcode the correct answer in it.
+
+$answer = array('A1' =>'Pakistan' , 'A2' => 'India' );
+$answer1= array('ans2'=>'India');
+	$answer2 = array('ans2'=>$answer);
+	echo $answer2['ans2']['A2'];
 
 
 //Write echo statement to print question to user.
-
-
+	echo "Who is the winner of ICC WORLD CUP 2012?";
 //Write echo statement to print all options
-
-
-
-
-
-
 //Write logic to find whether given answer is correct or not using if else.
-
-
-
   
 ?>
