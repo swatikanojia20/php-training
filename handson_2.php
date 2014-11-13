eb0ebb1a1c59956fb818ed345ac0150f2812fe09
Swati Kanojia <swatikanojia20@gmail.com>
2014-11-13 23:12:43 +0530

    Update handson_2.php

--- handson_2.php
+++ handson_2.php
@@ -5,7 +5,6 @@
 * Title  : PHP Hands-on - 2
 * Desc   : DiveIn (Constant, Control Structures)
 */
-
 /*
 * Write a PHP code to create a quiz. Store Answer in a Constant and using if-elseif-else controls display
 * appropriate message to the users.
@@ -14,48 +13,39 @@
 * Quiz Question : Who is the winner of ICC WORLD CUP 2011?
 * Options : Pakistan, Sri Lanka, India, England 
 */
-
 //Define a constant below
-
-
+define(ANSWER, India)
 //Declare an answer variable and hardcode the correct answer in it.
 $answer = "India";
-
 //Write echo statement to print question to user.
 echo "Who is the winner of ICC WORLD CUP 2011?";
 echo "<br>";
 //Write echo statement to print all options
-
-Switch($answer){
-
- case 'Pakistan':
- 	echo "Pakistan is a wrong answer";
- 	break;
-
- 	case 'Sri Lanka':
- 	 echo "Sri lanka is a wrong answer";
- 		break;
- 		case 'India':
- 			echo "India is a right answer";
- 			break;
- 			case 'England':
- 				echo "England is a wrong answer";
- 				break;
-
-
-}
+echo "Pakistan";
 echo "<br>";
-
+echo "SriLanka";
+echo "<br>"
+echo "India";
+echo "<br>"
+echo "England";
+echo "<br>"
 //Write logic to find whether given answer is correct or not using if else.
-
 if ($answer=="Pakistan") {
+	echo "This answer is incorrect";
+}
+elseif ($answer=="SriLanka") {
+	echo "This answer is incorrect";
+}
+elseif ($answer=="India") {
 	echo "This answer is correct";
+}
+elseif ($answer=="England") {
+	echo "This answer is incorrect";
 }
 else{
 
-echo "This is a wrong answer";
+echo "Above are all wrong";
 
 }
-
+}
   
-?>
